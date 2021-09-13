ab31f486751ceea029c3799099ad5abc2a6f367c
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-09-13 12:37:31 +0600

    Design Optimization Left

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const loadProducts = () => {
+
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
@@ -8,22 +9,25 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+
   const allProducts = products.map((pd) => pd);
+  const allProductsContainer = document.getElementById("all-products");
   for (const product of allProducts) {
-    const image = product.images;
+    const { image, title, category, price, id } = product;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
-    document.getElementById("all-products").appendChild(div);
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100">
+        <img src=${image} class="card-img-top product-image" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">${title}</h5>
+          <p>Category: ${category}</p>
+          <h2>Price: $ ${price}</h2>
+          <button onclick="addToCart(${id},${price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+          <button id="details-btn" class="btn btn-danger">Details</button></div>
+        </div>
+    </div>`
+    allProductsContainer.appendChild(div);
   }
 };
 let count = 0;
@@ -32,12 +36,13 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,12 +51,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -78,3 +83,14 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
+//     div.innerHTML = `<div class="single-product">
+// <div>
+// <img class="product-image" src=${image}></img>
+//   </div>
+//   <h3>${title}</h3>
+//   <p>Category: ${category}</p>
+//   <h2>Price: $ ${price}</h2>
+//   <button onclick="addToCart(${id},${price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+//   <button id="details-btn" class="btn btn-danger">Details</button></div>
+//   `;
